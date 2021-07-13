5ada37f6ae039bacfef20dd2a20aa1682b1ec89a
Melissa Kipp <melissakipp.az@gmail.com>
2021-07-13 12:52:13 -0700

    structured homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,7 @@
-const burger = document.querySelector('#burger');
-// const sidemenu = document.querySelector('')
+const menuToggle = document.querySelector('#menuBtn')
+
+menuToggle.addEventListener('click', () => {
+  console.log('Open')
+  document.querySelector('#topNav').hidden = false;
+  document.querySelector('#menuBtn').hidden = true;
+}, true);
