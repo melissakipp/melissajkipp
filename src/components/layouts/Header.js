c03c0ceeb1baafbd0bfd8dc6569eb6383ada28d0
Melissa Kipp <melissakipp.az@gmail.com>
2023-02-13 07:52:43 -0700

    Skeleton page layout

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <p>MJK</p>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
