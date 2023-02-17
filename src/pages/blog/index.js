9c31fef0511f718d2207274e9a1fcd3f517631f8
Melissa Kipp <melissakipp.az@gmail.com>
2023-02-17 05:52:43 -0700

    Basic blog and posts setup

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-const Blog = () => {
+const Blog = ({posts}) => {
   return (
-    <div>Blog</div>
+    <main>
+      <h2>Blog Post</h2>
+      <section>
+        <ul>
+          <li>
+            {posts.map((post, index) => (
+              <h3>{post.frontmatter.title}</h3>
+            ))}
+            
+          </li>
+        </ul>
+      </section>
+      {/* <div className='posts'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div> */}
+    </main>
   );
 }
+
+
+export async function getStaticProps() {
+  // Get files from the posts dir src\pages\blog\posts\test.md
+  const files = fs.readdirSync(path.join('src/posts/'));
+
+  // Get slug and frontmatter from posts
+  const posts = files.map(filename => {
+  // Create slug
+  const slug = filename.replace('.md', '');
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join('src/posts', filename),
+      'utf-8'
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    }
+  });
+  
+  return {
+    props: {
+      posts,
+    }
+  }
+}
+
+
+// export async function getStaticProps() {
+//   // Get files from the posts dir src\pages\blog\posts\test.md
+//   const files = fs.readdirSync(path.join('posts'))
+
+//   // Get slug and frontmatter from posts
+//   const posts = files.map((filename) => {
+//     // Create slug
+//     const slug = filename.replace('.md', '')
+
+//     // Get frontmatter
+//     const markdownWithMeta = fs.readFileSync(
+//       path.join('posts', filename),
+//       'utf-8'
+//     )
+
+//     const { data: frontmatter } = matter(markdownWithMeta)
+
+//     return {
+//       slug,
+//       frontmatter,
+//     }
+//   })
+
+//   return {
+//     props: {
+//       posts: posts,
+//     },
+//   }
+// }
 
 export default Blog;
---
+++ src/posts/mentors-to-follow.md
@@ -0,0 +1,33 @@
+---
+layout: post
+title: "My Mentors List 2021"
+date: 2021-10-08 15:09:12 +0000
+categories: jekyll
+tags: Pinned
+author: "Melissa Kipp"
+description: >
+  Mentors for web development students. A list of my top 5 mentors that have helped me. I have learned through a self-study style of learning how to code. These are they people that I have kept a close eye on.
+image: /assets/images/arizona-gallery-4.webp
+image_alt: "Desert flower"
+---
+
+![desert flower](/assets/images/arizona-gallery-4.webp){: .center-img}
+
+The value of finding a mentor is a great foundation to shaping your own personal brand. Everyone needs a personal brand now a days. It is especially helpful to employer to get a sense of your character and personally. This helps manager see where you can fix within the team structure. While this is important, finding a online mentor to help guide you through personal life and career is underrated.
+
+1. [Rachael Andrew](https://rachelandrew.co.uk/) and [Smashing Magazine](https://www.smashingmagazine.com/author/rachel-andrew/)
+   - Accessibility
+   - CSS
+2. [Leonie Watson](https://tink.uk/)
+   - Accessibility
+   - Screen reader
+3. [Chris Coyier](https://chriscoyier.net/) and [CSS-Tricks](https://css-tricks.com/author/chriscoyier/)
+   - CSS
+   - WordPress
+4. [Linus Lee](https://github.com/thesephist)
+   - [Podcast interview The Changelog: Building Software for yourself](https://changelog.com/podcast/455)
+5. [Bruce Lawson](https://brucelawson.co.uk/)
+   - Accessibility advocate
+   - Browser compatibility
+   - Transcript for video is formatted beautifully
+   - Progressive Web Apps
