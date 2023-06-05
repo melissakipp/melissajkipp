e35473debb7bfb196c779aea1a1048efab02a756
Melissa Kipp <melissakipp.az@gmail.com>
2023-06-05 16:43:34 -0700

    Starting point

--- README.md
+++
@@ -1,2 +0,0 @@
-# Redesign
-[Next.js](https://nextjs.org/)
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/page.module.scss';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Melissa's Portfolio</h1>
+    </main>
+  );
+}
