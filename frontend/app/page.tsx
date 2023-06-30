08f71769a44b4a720a65d47aa0e85b88b653e707
Melissa Kipp <melissakipp.az@gmail.com>
2023-06-29 19:26:14 -0700

    Code clean up

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
-import styles from '../styles/page.module.scss';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Melissa Kipp</h1>
-      <Link href="/projects">Projects</Link>
+      <Link href='/projects'>Projects</Link>
     </main>
   );
 }
