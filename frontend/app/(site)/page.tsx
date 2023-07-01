ada82b15a2e7fcb7af6e61d8daa4a932d01a7f80
Melissa Kipp <melissakipp.az@gmail.com>
2023-07-01 10:17:02 -0700

    Basic start

--- frontend/app/(site)/page.tsx
+++ frontend/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'Melissa J Kipp | Home',
@@ -7,12 +8,15 @@ export const metadata = {
 export default function Home() {
   return (
     <article className='min-h-[72vh] lg:max-w-7xl m-auto p-5'>
-      <h1 className='text-7xl font-extrabold my-20'>
-        Hello, I&apos;m{' '}
-        <span className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
-          Melissa Kipp
-        </span>
-      </h1>
+      <figure>
+        <Image
+          src='/images/josefina-di-battista-sym1nRsVkAY-unsplash.jpg'
+          alt='Melissa J Kipp'
+          width={300}
+          height={167}
+        />
+      </figure>
+      <h1 className='text-3xl lg:text-7xl font-extrabold'>Melissa J Kipp</h1>
       <p className='my-3 text-xl text-gray-600'>Check out my projects!</p>
       <Link href='/projects'>Projects</Link>
     </article>
