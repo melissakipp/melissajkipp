bef93c2107e476b018bee6ae44445e1fd7c480a4
Melissa Kipp <melissakipp.az@gmail.com>
2023-07-01 08:15:53 -0700

    Modify layout

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -7,38 +7,35 @@ export default async function Header() {
   const pages = await getPages();
   return (
     <>
-      {/* className='flex justify-between items-center max-w-5xl mx-auto p-5' */}
-      <header className='bg-blue-200 w-full py-5'>
-        <div className='max-w-screen-xl mx-auto'>
-          <div className='flex justify-between'>
-            <Image
-              src='/images/full-logo_bk_version-1.svg'
-              alt='Melissa Kipp'
-              width={125}
-              height={100}
-            />
-            <nav className='text-xl max-w-full'>
-              <ul className='flex justify-between items-center p-5'>
-                <li className='mx-5'>
-                  <Link className='hover:underline' href='/'>
-                    Home
+      <header className='min-h-[14vh] bg-blue-200 w-full p-3 flex items-center'>
+        <div className='flex justify-between max-w-screen-xl mx-auto w-full'>
+          <Image
+            src='/images/full-logo_bk_version-1.svg'
+            alt='Melissa Kipp'
+            width={125}
+            height={100}
+          />
+          <nav className='text-xl max-w-full'>
+            <ul className='flex justify-between items-center p-5'>
+              <li className='mx-5'>
+                <Link className='hover:underline' href='/'>
+                  Home
+                </Link>
+              </li>
+              <li className='mx-5'>
+                <Link className='hover:underline' href='/projects'>
+                  Projects
+                </Link>
+              </li>
+              {pages.map((page) => (
+                <li className='mx-5' key={page._id}>
+                  <Link className='hover:underline' href={`/${page.slug}`}>
+                    {page.title}
                   </Link>
                 </li>
-                <li className='mx-5'>
-                  <Link className='hover:underline' href='/projects'>
-                    Projects
-                  </Link>
-                </li>
-                {pages.map((page) => (
-                  <li className='mx-5' key={page._id}>
-                    <Link className='hover:underline' href={`/${page.slug}`}>
-                      {page.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
+              ))}
+            </ul>
+          </nav>
         </div>
       </header>
     </>
