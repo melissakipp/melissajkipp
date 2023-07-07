6b31ac6bce28be7483a5d561d5cadf2091f6cc74
Melissa Kipp <melissakipp.az@gmail.com>
2023-07-07 13:43:51 -0700

    Fix useCdn setting

--- frontend/sanity/config/client-config.ts
+++ frontend/sanity/config/client-config.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId, useCdn } from '../env';
+import { apiVersion, dataset, projectId } from '../env';
 
 export const clientConfig = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn,
+  useCdn: false,
 });
