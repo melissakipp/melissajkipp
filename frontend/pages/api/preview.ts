10fa361803cd12c1da2d6ab1bda6cf533c9dfe93
Melissa Kipp <melissakipp.az@gmail.com>
2023-07-10 11:40:30 -0700

    Add comment

--- frontend/pages/api/preview.ts
+++ frontend/pages/api/preview.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function preview(req: NextApiRequest, res: NextApiResponse) {
-  res.setPreviewData({});
+  res.setPreviewData({}); // can be set with a token
   res.writeHead(307, { Location: '/' });
   res.end();
 }
