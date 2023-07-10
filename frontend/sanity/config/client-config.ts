942ba24bb37eb02e1c70f0a5bbd7cb3022c57c34
Melissa Kipp <melissakipp.az@gmail.com>
2023-07-10 12:19:29 -0700

    Reconfig for preview-kit

--- frontend/sanity/config/client-config.ts
+++ frontend/sanity/config/client-config.ts
@@ -1,8 +1,21 @@
-import { createClient } from 'next-sanity';
+import { createClient } from '@sanity/preview-kit/client';
 
 import { apiVersion, dataset, projectId } from '../env';
 
+const config = {
+  // ...base config options
+
+  // Required: when "encodeSourceMap" is enabled
+  // Set it to relative or absolute URL of your Sanity Studio
+  studioUrl: '/admin', // or 'https://your-project-name.sanity.studio'
+
+  // Required: for encoded metadata from Content Source Maps
+  // 'auto' is the default, you can also use `true` or `false`
+  encodeSourceMap: 'auto',
+};
+
 export const clientConfig = createClient({
+  ...config,
   apiVersion,
   dataset,
   projectId,
