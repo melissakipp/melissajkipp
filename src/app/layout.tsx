4b7b7da4e4bbe3ce8f26007bb5a42445cdc2c162
melissakipp <melissakipp.az@gmail.com>
2024-10-16 07:57:17 -0700

    Add Roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "@next/font/google";
 import "./globals.css";
 
 import Footer from "@/src/components/layouts/Footer";
 import Header from "@/src/components/layouts/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Melissa J Kipp | Frontend Developer",
@@ -19,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
         <Header />
         <main>
           {children}
