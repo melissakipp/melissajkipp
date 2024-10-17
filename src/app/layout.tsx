75d69aa48abd1f5e678b884f16b20fbd9889ad2e
melissakipp <melissakipp.az@gmail.com>
2024-10-16 20:36:15 -0700

    Removed @next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
 import Footer from "@/src/components/layouts/Footer";
