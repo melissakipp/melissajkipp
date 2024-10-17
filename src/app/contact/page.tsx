9fc5b22e3b3f5603c86eda840f004fae8318e3f6
melissakipp <melissakipp.az@gmail.com>
2024-10-16 20:43:56 -0700

    Code clean up

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from '../../components/Forms/ContactForm';
 
 export default function Contact() {
     return (
-      <article className="h-[100vh]">
+      <article className='h-[100vh]'>
         <h1 className='sr-only'>Contact Page</h1>
         <ContactForm />
       </article>
