6fc4c27cc8f1f76314fe16ef932a01658d1fdb1c
melissakipp <melissakipp.az@gmail.com>
2024-10-22 08:30:51 -0700

    Add CTA schema for component

---
+++ sanity/schemaTypes/callToAction.ts
@@ -0,0 +1,48 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'callToAction',
+  title: 'CTA',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'headline',
+      title: 'Headline',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        }
+      ]
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        },
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+})
